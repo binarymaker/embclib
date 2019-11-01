8b8ec4f6c485c3042f477b873ef28b510cc18b96
binarymamker <importfunfomcode@gmail.com>
2019-11-01 19:12:46 +0530

    REG merge added

--- source/regctrl.h
+++ source/regctrl.h
@@ -46,10 +46,13 @@
 #define REG_Clear(reg, val)                                   ((reg) &= ~(val))
 #define REG_Write(reg, val)                                     ((reg) = (val))
 #define REG_Read(reg)                                                   ((reg))
-#define REG_Update(reg)                                        ((reg) |= (val))  
+#define REG_Update(reg)                                        ((reg) |= (val))
+#define REG_Merge(reg, val, mask)                                             \
+                                 ((reg) = ((reg) & ~(mask)) | ((val) & (mask)))
 #define REG_GroupRead(reg, bitpos, len)                                       \
                                     (((reg) >> (bitpos)) & BIT_MaskFill((len)))
-#define REG_GroupWrite(reg, val, bitpos)          (((reg) = (val) << (bitpos)))
+#define REG_GroupWrite(reg, val, bitpos)            ((reg) = (val) << (bitpos))
+
 #define REG_Shift(val, bitpos)                              ((val) << (bitpos))
 #define REG_RightShift(val, bitpos)                         ((val) >> (bitpos))
 #define REG_Modify(reg, setmask, clearmask)                                   \
