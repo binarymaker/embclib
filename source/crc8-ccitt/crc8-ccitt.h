eb4c80805b3f42322b6fa89f120f0717e31fab45
binarymaker <importfunfromcode@gmail.com>
2019-12-28 10:40:55 +0000

    crc8 library template added

---
+++ source/crc8-ccitt/crc8-ccitt.h
@@ -0,0 +1,47 @@
+/**\cond
+  ******************************************************************************
+  * ______  _                             ___  ___        _               
+  * | ___ \(_)                            |  \/  |       | |              
+  * | |_/ / _  _ __    __ _  _ __  _   _  | .  . |  __ _ | | __ ___  _ __ 
+  * | ___ \| || '_ \  / _` || '__|| | | | | |\/| | / _` || |/ // _ \| '__|
+  * | |_/ /| || | | || (_| || |   | |_| | | |  | || (_| ||   <|  __/| |   
+  * \____/ |_||_| |_| \__,_||_|    \__, | \_|  |_/ \__,_||_|\_\\___||_|   
+  *                                 __/ |                                 
+  *                                |___/                                  
+  *                                                                       
+  * Copyright (C) 2019 Binary Maker - All Rights Reserved
+  *
+  * This program and the accompanying materials are made available
+  * under the terms described in the LICENSE file which accompanies
+  * this distribution.
+  * Written by Binary Maker <https://github.com/binarymaker>
+  ******************************************************************************
+  \endcond*/
+
+#ifndef CRC8_CCITT_4a53f9d6_295e_11ea_926e_72fa06eae96d
+#define CRC8_CCITT_4a53f9d6_295e_11ea_926e_72fa06eae96d
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/**
+ * \brief Source file version tag
+ *        
+ *        version info: [15:8] main [7:0] beta
+ */
+#define __CRC8_CCITT_VERSION      (0x0001u)
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CRC8_CCITT_4a53f9d6_295e_11ea_926e_72fa06eae96d */
+
